ccce681fb2e380ee586f282fb619db0164b366ed
jasen_huang <hyl1718@163.com>
2015-12-12 10:45:49 +0800

    init by hyl at 2015.12.12

---
+++ project/obj/local/armeabi/objs/serial_port/SerialPort.o.d
@@ -0,0 +1,146 @@
+/home/eric/Desktop/android-ndk-r4b/apps/android-serialport-api-read-only/android-serialport-api/project/obj/local/armeabi/objs/serial_port/SerialPort.o: \
+ /home/eric/Desktop/android-ndk-r4b/apps/android-serialport-api-read-only/android-serialport-api/project/jni/SerialPort.c \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/termios.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/cdefs.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/ioctl.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/ioctl.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/ioctl.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm-generic/ioctl.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/ioctls.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/termbits.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/ioctl_compat.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/types.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/stdint.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/_types.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/machine/_types.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/posix_types.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/stddef.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/compiler.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/posix_types.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/types.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/types.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/machine/kernel.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/termios.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/termios.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/unistd.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/select.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/time.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/time.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/sysconf.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/capability.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/pathconf.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/stat.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/stat.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/endian.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/endian.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/fcntl.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/fcntl.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/fcntl.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm-generic/fcntl.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/string.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/malloc.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/jni.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/errno.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/errno.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/errno.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm-generic/errno.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm-generic/errno-base.h \
+ /home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/android/log.h
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/termios.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/cdefs.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/ioctl.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/ioctl.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/ioctl.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm-generic/ioctl.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/ioctls.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/termbits.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/ioctl_compat.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/types.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/stdint.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/_types.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/machine/_types.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/posix_types.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/stddef.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/compiler.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/posix_types.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/types.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/types.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/machine/kernel.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/termios.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/termios.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/unistd.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/select.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/time.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/time.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/sysconf.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/capability.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/pathconf.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/stat.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/stat.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/endian.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/sys/endian.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/fcntl.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/fcntl.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/fcntl.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/string.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/malloc.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/jni.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/errno.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/linux/errno.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm/errno.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm-generic/errno.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/home/eric/Desktop/android-ndk-r4b/build/platforms/android-5/arch-arm/usr/include/android/log.h:
